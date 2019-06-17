3fa7f957ce738d4b65876c480112ad5926eab311
ThomasPoot <35296806+ThomasPoot@users.noreply.github.com>
2019-06-17 15:24:54 +0100

    Set the main class, with dummy code

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "address-index-acceptance-tests"
 version := "0.1"
 
 scalaVersion := "2.13.0"
+
+mainClass := Some("uk.gov.ons.addressindex.acceptancetests.Main")
---
+++ src/main/scala/uk/gov/ons/addressindex/acceptancetests/Main.scala
@@ -0,0 +1,9 @@
+package uk.gov.ons.addressindex.acceptancetests
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Starting acceptance tests")
+    println("No tests to run")
+    println("Ending acceptance tests")
+  }
+}
